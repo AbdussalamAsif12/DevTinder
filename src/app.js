a622bb67942250e5ee67f636458e0d3b9fec20eb
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-01-08 01:04:21 +0500

    Exploring GET,PUT,POST,PATCH

--- src/app.js
+++ src/app.js
@@ -2,12 +2,19 @@ const express = require("express");
 
 const app = express();
 
-app.use("/",(req,res)=>{
-    res.send("Admin Dashboard")
-})
+app.get("/dataEnter", (req, res) => {
+  res.send({  task: "GET", message: "oK! Done"  });
+});
+app.post("/dataEnter", (req, res) => {
+  res.send({ task: "POST", message: "Data Saved Successfully" });
+});
+app.put("/dataEnter", (req, res) => {
+  res.send({ task: "PUT", message: "Data Update Successfully" });
+});
+app.delete("/dataEnter", (req, res) => {
+  res.send({ task: "DELETE", message: "Data Deleted Successfully" });
+});
 
-
-app.listen(3000,()=>{
-    console.log(`SERVER is successfully listing on 3000`);
-    
-})
+app.listen(3000, () => {
+  console.log(`SERVER is successfully listing on 3000`);
+});
