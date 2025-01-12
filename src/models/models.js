3452107b1ed870b36faf2a7b6265ecaa1adbcf66
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-01-12 21:23:05 +0500

    model validations

--- src/models/models.js
+++ src/models/models.js
@@ -5,21 +5,26 @@ const userSchema = new mongoose.Schema(
   {
     firstName: {
       type: String,
+      trim:true,
+      lowercase:true,
       minLength: [4, "First name must be at least 4 characters long"],
       maxLength: [50, "First name must not exceed 50 characters"],
       required: [true, "Enter First Name"],
-      match: [/^[a-zA-Z]+$/, "First name should contain only alphabets"],
+      match: [/^[a-zA-Z\s]+$/, "First name should contain only alphabets"]
     },
     lastName: {
       type: String,
+      trim:true,
+      lowercase:true,
       minLength: [4, "Last name must be at least 4 characters long"],
       maxLength: [50, "Last name must not exceed 50 characters "],
-      match: [/^[a-zA-Z]+$/, "Last name should contain only alphabets"],
+      match: [/^[a-zA-Z\s]+$/, "Last name should contain only alphabets"],
     },
     emailId: {
       type: String,
+      trim:true,
       lowercase: true,
-      required: true,
+      required: [true,"Email is required"],
       unique: true,
       trim: true,
       validate(value) {
@@ -33,22 +38,22 @@ const userSchema = new mongoose.Schema(
       required: true,
       validate(value) {
         if (!validator.isStrongPassword(value)) {
-          throw new Error("Enter a Strong Password");
+          throw new Error(`Enter a Strong Password ${value}`);
         }
       },
     },
     age: {
       type: Number,
       required: [true, "Age is required"],
-      min: [0, "Age must be at least 0"],
+      min: [18, "Age must be at least 18"],
       max: [120, "Age must not exceed 120"],
     },
     gender: {
       type: String,
-      validate(value) {
-        if (!["male", "female", "others"].includes(value)) {
-          throw new Error("Gender data is not valid");
-        }
+      lowercase:true,
+      enum: {
+        values: ["male", "female", "other"],
+        message: `{VALUE} is not a valid gender type`,
       },
     },
     photoUrl: {
@@ -63,6 +68,7 @@ const userSchema = new mongoose.Schema(
     about: {
       type: String,
       required: true,
+      trim:true,
       default: "Tell us about yourself...",
       validate(value) {
         if (value.length < 10) {
