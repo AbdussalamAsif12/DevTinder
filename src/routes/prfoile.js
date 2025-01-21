b310993cb31046c8cdf1e1170e076bf3d4bafb3c
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-01-21 21:28:56 +0500

    password change

--- src/routes/prfoile.js
+++ src/routes/prfoile.js
@@ -1,8 +1,8 @@
 const express = require("express");
 const profileRouter = express.Router();
-
 const { userAuth } = require("../middleware/auth");
 const { validateEditProfileData } = require("../utils/validation");
+const bcrypt = require("bcrypt");
 
 profileRouter.get("/profile/view", userAuth, async (req, res) => {
   try {
@@ -29,6 +29,39 @@ profileRouter.patch("/profile/edit", userAuth, async (req, res) => {
     res.json({
       message: `${loggedInUser.firstName}, your profile updated successfuly`,
       data: loggedInUser,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR : " + err.message);
+  }
+});
+
+profileRouter.patch("/profile/password", userAuth, async (req, res) => {
+  try {
+    const { oldPassword, newPassword } = req.body;
+    // Validate input
+    if (!oldPassword || !newPassword) {
+      return res
+        .status(400)
+        .json({ error: "Both old and new passwords are required." });
+    }
+
+    const isPasswordValid = await req.user.validatePassword(oldPassword);
+    if (!isPasswordValid) {
+      return res.status(400).json({ error: "Incorrect old password." });
+    }
+
+    if (await bcrypt.compare(newPassword, req.user.password)) {
+      return res
+        .status(400)
+        .json({
+          error: "New password must not be the same as the old password.",
+        });
+    }
+
+    req.user.password = await bcrypt.hash(newPassword, 10);
+    await req.user.save();
+    res.json({
+      message: `${req.user.firstName}, your password has been updated successfully.`,
     });
   } catch (err) {
     res.status(400).send("ERROR : " + err.message);
