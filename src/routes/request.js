c2926c7ab7c79474660f8fbb303c669bd3a09232
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-01-24 02:23:23 +0500

    change in request #2

--- src/routes/request.js
+++ src/routes/request.js
@@ -3,31 +3,45 @@ const request = express.Router();
 const ConnectionRequest = require("../models/connection.model");
 const { userAuth } = require("../middleware/auth");
 
-request.post(
-  "/request/send/:status/:toUserId",
-  userAuth,
-  async (req, res) => {
-    try {
-      const fromUserId = req.user._id;
-      const toUserId = req.params.toUserId;
-      const status = req.params.status;
+request.post("/request/send/:status/:toUserId", userAuth, async (req, res) => {
+  try {
+    const fromUserId = req.user._id;
+    const toUserId = req.params.toUserId;
+    const status = req.params.status;
 
-      const connectionRequest = new ConnectionRequest({
-        fromUserId,
-        toUserId,
-        status,
-      });
-
-      const data = await connectionRequest.save();
-
-      res.json({
-        message: "Connection Request Sent Successfully!",
-        data,
-      });
-    } catch (err) {
-      res.status(400).send("ERROR: " + err.message);
+    const allowedStatus = ["ignored", "interested"];
+    if (!allowedStatus.includes(status)) {
+      return res.status(400).json(`message : Invalid Status Type ${status}`);
     }
+
+    const existingConnectionRequest = await ConnectionRequest.findOne({
+      $or: [
+        { fromUserId, toUserId },
+        { fromUserId: toUserId, toUserId: fromUserId },
+      ],
+    });
+
+    if (existingConnectionRequest) {
+      return res
+        .status(400)
+        .send({ message: "Connection Request Already Exist" });
+    }
+
+    const connectionRequest = new ConnectionRequest({
+      fromUserId,
+      toUserId,
+      status,
+    });
+
+    const data = await connectionRequest.save();
+
+    res.json({
+      message: "Connection Request Sent Successfully!",
+      data,
+    });
+  } catch (err) {
+    res.status(400).send("ERROR: " + err.message);
   }
-);
+});
 
 module.exports = request;
