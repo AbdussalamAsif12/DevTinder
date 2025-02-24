7a2336885fb793bef98292875827c38ccd0981bc
Abdulislam Asif <abdusslamasif8955@gmail.com>
2025-02-25 02:10:26 +0500

    Some Bugs

--- config/database.js
+++ config/database.js
@@ -2,7 +2,9 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   mongoose.connect(
-    "mongodb+srv://abdusslamasif8955:1Pshs6Pgq7oqospl@namastenodeakshay.zoybo.mongodb.net/devTinder"
+    "mongodb+srv://abdusslamasif8955:1Pshs6Pgq7oqospl@namastenodeakshay.zoybo.mongodb.net/devTinder",
+    
+    
   );
 };
 
